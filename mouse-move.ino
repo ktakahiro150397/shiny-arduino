0d0b9d12ab44ef8c0ffd86e870ab1b6447d71fe3
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-08-10 17:24:56 +0900

    ランダム再生OK

---
+++ mouse-move.ino
@@ -0,0 +1,98 @@
+
+const int monitorWidth = 3840;
+const int monitorHeight = 2160;
+
+const int initializeValue = 9999;
+
+void moveMouse(int x, int y)
+{
+    moveMouseForX(x);
+    moveMouseForY(y);
+}
+
+void moveMouseForX(int x)
+{
+    int current_x = x;
+
+    if (current_x > 0)
+    {
+        while (current_x > 0)
+        {
+            if (current_x > 127)
+            {
+                current_x -= 127;
+                Mouse.move(127, 0, 0);
+            }
+            else
+            {
+                Mouse.move(current_x, 0, 0);
+                current_x = 0;
+            }
+        }
+    }
+    else
+    {
+        while (current_x < 0)
+        {
+            if (current_x < -127)
+            {
+                current_x += 127;
+                Mouse.move(-127, 0, 0);
+            }
+            else
+            {
+                Mouse.move(current_x, 0, 0);
+                current_x = 0;
+            }
+        }
+    }
+}
+
+void moveMouseForY(int y)
+{
+    int current_y = y;
+
+    if (current_y > 0)
+    {
+        while (current_y > 0)
+        {
+            if (current_y > 127)
+            {
+                current_y -= 127;
+                Mouse.move(0, 127, 0);
+            }
+            else
+            {
+                Mouse.move(0, current_y, 0);
+                current_y = 0;
+            }
+        }
+    }
+    else
+    {
+        while (current_y < 0)
+        {
+            if (current_y < -127)
+            {
+                current_y += 127;
+                Mouse.move(0, -127, 0);
+            }
+            else
+            {
+                Mouse.move(0, current_y, 0);
+                current_y = 0;
+            }
+        }
+    }
+}
+
+/// @brief 現在のマウスの位置をモニターの左上に移動する
+void mousePositionInitialize()
+{
+    moveMouse(initializeValue, initializeValue);
+}
+
+void mvButtonHeightInitialize()
+{
+    moveMouse(0, -50);
+}
--- shiny-aruduino.ino
+++ shiny-aruduino.ino
@@ -1,21 +1,56 @@
 #include "Mouse.h"
 
+const int notification_led_port = 13;
+
 void setup()
 {
     // put your setup code here, to run once:
-    pinMode(13, OUTPUT);
+    pinMode(notification_led_port, OUTPUT);
+    digitalWrite(notification_led_port, LOW);
 
+    // シリアルポートを開く
+    Serial.begin(9600);
+
+    // Mouseライブラリの初期化
     Mouse.begin();
+
+    // 初期化完了を通知
+    flashLED(notification_led_port);
+    flashLED(notification_led_port);
+
+    randomSeed(analogRead(0));
 }
+
+// マウス移動処理中かどうかのフラグ
+bool isProcessing = false;
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
-    flashLED(13);
+    if (Serial.available() > 0)
+    {
+        int x = Serial.parseInt();
 
-    // Mouse.move(100, 200, 0);
+        if (x == 1 && !isProcessing)
+        {
+            // MV再生処理完了
+            flashLED(notification_led_port);
+            flashLED(notification_led_port);
 
-    delay(5 * 1000);
+            // ここでマウスを動かす
+
+            // 処理中フラグ設定
+            isProcessing = true;
+
+            moveMouseForStartingMv();
+
+            // MV再生処理完了
+            flashLED(notification_led_port);
+            flashLED(notification_led_port);
+
+            // フラグクリア
+            isProcessing = false;
+        }
+    }
 }
 
 void flashLED(int port)
@@ -23,4 +58,34 @@ void flashLED(int port)
     digitalWrite(port, HIGH);
     delay(100);
     digitalWrite(port, LOW);
+}
+
+void moveMouseForStartingMv()
+{
+    // カーソル位置初期化
+    mousePositionInitialize();
+    mvButtonHeightInitialize();
+
+    // ランダムボタン押下
+    int randNumber = random(1, 10); // 10から9の乱数を生成
+    moveMouse(-800, 0);
+
+    for (int i = 0; i < randNumber; i++)
+    {
+        Mouse.click();
+        delay(0.8 * 1000);
+    }
+
+    // MV再生ボタン押下
+    moveMouse(500, 0);
+    Mouse.click();
+    delay(2 * 1000);
+
+    // スタートボタン押下
+    moveMouse(200, 0);
+    Mouse.click();
+    delay(1 * 1000);
+
+    // マウスを元の位置に戻す
+    mousePositionInitialize();
 }
